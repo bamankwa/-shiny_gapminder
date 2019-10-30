8d2ea0f9e1eec45d1d87ce8c0c3fd81bc68a9624
bamankwa <bamankwa@vols.utk.edu>
2019-10-30 15:01:05 -0400

    loading shiny, tidyverse, gapminder packages

---
+++ homework8.R
@@ -0,0 +1,13 @@
+
+########
+# 
+#   Visualization of Gapminder Dataset With Shiny App 
+#
+##############################################################################
+
+ #  Library required packages ..shiny, tidyverse, gapminder
+
+library(shiny)
+library(tidyverse)
+library(gapminder)
+
